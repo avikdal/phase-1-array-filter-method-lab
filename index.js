b647ba79f599ea944c9400f39b115dbe696341c8
amanda vikdal <vikdal.amanda@gmail.com>
2022-11-15 13:46:29 -0700

    commit tree a036ba0e9d82fb73d571a498d0f63adec9e4a463

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution here
+function findMatching(arrayOfDrivers, string){
+    let newDrivers = arrayOfDrivers.filter(function (driver){
+        return driver.toUpperCase() === string.toUpperCase()
+        console.log("driver", driver);
+    });
+    console.log("newDrivers", newDrivers);
+    return newDrivers;
+}
+
+function fuzzyMatch(arrayOfDrivers, string){
+    return arrayOfDrivers.filter(function (driver){
+        return driver.toLowerCase().substring(0, string.length) === string.toLowerCase()
+    })
+    // let newDrivers = arrayOfDrivers.filter(function (driver){
+    //     return driver.toLowerCase().startsWith(0, string.length) === string.toLowerCase()
+    //     console.log("driver", driver);
+    // });
+    // return newDrivers;
+}
+
+function matchName(arrayOfDrivers, string) {
+    return arrayOfDrivers.filter(function (driver) {
+       return driver.name === string
+    });
+}
+
